00cb029e4a04df6e6bea7c3ff1f593546cae1fc1
Izumi Zama <themax67@gmail.com>
2019-07-29 12:40:11 +0900

    Updates

---
+++ precache-manifest.59a6e9eb000d0c1926e71a69ce5b41c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc5c1c34ac892986d3f444d3520ca274",
+    "url": "/my-todo-app/index.html"
+  },
+  {
+    "revision": "df99289374eef9247ccf",
+    "url": "/my-todo-app/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "3c28c67234f869c260df",
+    "url": "/my-todo-app/static/js/2.56b10b1c.chunk.js"
+  },
+  {
+    "revision": "df99289374eef9247ccf",
+    "url": "/my-todo-app/static/js/main.15892f2a.chunk.js"
+  },
+  {
+    "revision": "b4a2e2a3f9b01507921b",
+    "url": "/my-todo-app/static/js/runtime~main.67b3e01c.js"
+  }
+]);
